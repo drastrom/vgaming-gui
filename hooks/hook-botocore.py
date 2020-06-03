c7fb3aeb6270d54cd92e8e39f088293f18c4a2de
Jeremy Drake <github@jdrake.com>
2020-06-02 22:47:55 -0700

    Add pyinstaller support for building windows exe.
    Added custom botocore hook to exclude data files that we don't use (only
    the lastest ec2 api), and manifest for common controls and dpi
    awareness (though the latter has not really been tested).

---
+++ hooks/hook-botocore.py
@@ -0,0 +1,39 @@
+#-----------------------------------------------------------------------------
+# Copyright (c) 2015-2020, PyInstaller Development Team.
+#
+# Distributed under the terms of the GNU General Public License (version 2
+# or later) with exception for distributing the bootloader.
+#
+# The full license is in the file COPYING.txt, distributed with this software.
+#
+# SPDX-License-Identifier: (GPL-2.0-or-later WITH Bootloader-exception)
+#-----------------------------------------------------------------------------
+#
+# Botocore is a low-level interface to a growing number of Amazon Web Services.
+# Botocore serves as the foundation for the AWS-CLI command line utilities. It
+# will also play an important role in the boto3.x project.
+#
+# The botocore package is compatible with Python versions 2.6.5, Python 2.7.x,
+# and Python 3.3.x and higher.
+#
+# https://botocore.readthedocs.org/en/latest/
+#
+# Tested with botocore 1.4.36
+
+import os.path
+from PyInstaller.utils.hooks import get_package_paths
+from PyInstaller.compat import is_py2
+from PyInstaller.utils.hooks import is_module_satisfies
+
+if is_module_satisfies('botocore >= 1.4.36'):
+    if is_py2:
+        hiddenimports = ['HTMLParser']
+    else:
+        hiddenimports = ['html.parser']
+
+
+pkg_base, pkg_dir = get_package_paths('botocore')
+
+datas = [(os.path.join(pkg_dir, "cacert.pem"), 'botocore'),
+         (os.path.join(pkg_dir, "data", "*.json"), 'botocore/data'),
+         (os.path.join(pkg_dir, "data", "ec2", "2016-11-15"), 'botocore/data/ec2/2016-11-15')]
